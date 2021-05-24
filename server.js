24799d4a22925027527cc0de25c94fcb132cb11d
Nathan Morrissey <nmorriss7109@gmail.com>
2021-05-24 14:25:39 -0700

    small change 3

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ import { Server } from "socket.io";
 import { addUser, deleteUser, getUser, getUsers } from "./users.js";
 import { error, time } from "console";
 import SpotifyWebApi from "spotify-web-api-node";
-import path from 'path';
+// import path from 'path';
 
 const main = async () => {
   const app = express();
@@ -18,12 +18,12 @@ const main = async () => {
 
   app.use(cors());
 
-  if (process.env.NODE_ENV === "production") {
-    app.use(express.static("build"));
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname,  "client", "public", "index.html"));
-    });
-  }
+  // if (process.env.NODE_ENV === "production") {
+  //   app.use(express.static("build"));
+  //   app.get("*", (req, res) => {
+  //     res.sendFile(path.resolve(__dirname,  "client", "public", "index.html"));
+  //   });
+  // }
 
   var spotifyApi = new SpotifyWebApi({
     clientId: process.env.SPOTIFY_CLIENT_ID,
