34fd3c99acf48a6cd3b208a5db13e8b5f33d537d
Nathan Morrissey <nmorriss7109@gmail.com>
2021-05-24 14:41:21 -0700

    small change 6

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ const main = async () => {
 
   if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/public"));
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname,  "client", "public", "index.html"));
-    });
+    // app.get("*", (req, res) => {
+    //   res.sendFile(path.resolve(__dirname,  "client", "public", "index.html"));
+    // });
   }
 
   var spotifyApi = new SpotifyWebApi({
