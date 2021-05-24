e14b7331a79736128017c079bf28a8f4386a9629
Nathan Morrissey <nmorriss7109@gmail.com>
2021-05-24 14:11:58 -0700

    small change

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const main = async () => {
   if (process.env.NODE_ENV === "production") {
     app.use(express.static("build"));
     app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+      res.sendFile(path.resolve(__dirname,  "client", "public", "index.html"));
     });
   }
 
