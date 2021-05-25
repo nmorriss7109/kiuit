b7b10711855ca15cd280c706dc1d2cea55dd633b
Nathan Morrissey <nmorriss7109@gmail.com>
2021-05-24 17:27:45 -0700

    small change 7

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const main = async () => {
   app.use(cors());
 
   if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/public"));
+    app.use(express.static("client/build"));
     // app.get("*", (req, res) => {
     //   res.sendFile(path.resolve(__dirname,  "client", "public", "index.html"));
     // });
