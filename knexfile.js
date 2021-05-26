2779f83c71790e019ec65671c46ac2b74c64e0a3
Nathan Morrissey <nmorriss7109@gmail.com>
2021-05-25 18:18:27 -0700

    Trying to get db to work! #3

--- knexfile.js
+++ knexfile.js
@@ -26,7 +26,7 @@ export default {
       // database: 'dbb4nhfchi0ml9',
       // user: 'hyvztknsknozgm',
       // password: '8282c0759bc29a3756e673ccde55ae4795a2dba93d3b17564904893a8c3ffb7e',
-      // ssl: { rejectUnauthorized: false }
+      ssl: { rejectUnauthorized: false }
     },
     debug: true,
     migrations: {
@@ -35,7 +35,6 @@ export default {
     seeds: {
       directory: './db/seeds/production',
     },
-    ssl: true,
   }
 
 };
