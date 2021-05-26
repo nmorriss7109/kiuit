52ca1a8352f3274d5984b46a45932384f22c4eca
Nathan Morrissey <nmorriss7109@gmail.com>
2021-05-25 18:23:33 -0700

    Trying to get db to work! #4

--- knexfile.js
+++ knexfile.js
@@ -21,11 +21,11 @@ export default {
   production: {
     client: 'pg',
     connection: {
-      connectionString: process.env.DATABASE_URL,
-      // host: 'ec2-18-211-97-89.compute-1.amazonaws.com',
-      // database: 'dbb4nhfchi0ml9',
-      // user: 'hyvztknsknozgm',
-      // password: '8282c0759bc29a3756e673ccde55ae4795a2dba93d3b17564904893a8c3ffb7e',
+      // connectionString: process.env.DATABASE_URL,
+      host: 'ec2-18-211-97-89.compute-1.amazonaws.com',
+      database: 'dbb4nhfchi0ml9',
+      user: 'hyvztknsknozgm',
+      password: '8282c0759bc29a3756e673ccde55ae4795a2dba93d3b17564904893a8c3ffb7e',
       ssl: { rejectUnauthorized: false }
     },
     debug: true,
